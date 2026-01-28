f5fa7709291ad159123f0d6bd9416066f40223c1
Álvaro Braz <alvarobraz83@gmail.com>
2026-01-27 23:50:17 -0300

    feat: create-reads-volumes-multiple-containers-with-docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-devops-docker-api',
+      port: 3306,
+      username: 'admin',
+      password: 'root',
+      database: 'devops-docker-api-db',
+      autoLoadEntities: true,
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
