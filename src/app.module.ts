88c17deb43fb2c84eca26a7da2057d65389f1886
Álvaro Braz <alvarobraz83@gmail.com>
2026-02-09 17:30:06 -0300

    feat: deploy-application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql-devops-docker-api',
-      port: 3306,
-      username: 'admin',
-      password: 'root',
-      database: 'devops-docker-api-db',
-      autoLoadEntities: true,
-      synchronize: true,
-      retryAttempts: 10,
-      retryDelay: 3000,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'mysql-devops-docker-api',
+    //   port: 3306,
+    //   username: 'admin',
+    //   password: 'root',
+    //   database: 'devops-docker-api-db',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   retryAttempts: 10,
+    //   retryDelay: 3000,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
