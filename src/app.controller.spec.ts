0a6dcd2f2eabbcf8dd2c453628a93944b9804c29
Álvaro Braz <alvarobraz83@gmail.com>
2026-02-09 17:40:07 -0300

    feat: logs-and-monitoring

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Olá DEV DEVOPS!"', () => {
+      expect(appController.getHello()).toBe('Olá DEV DEVOPS!');
     });
   });
 });
